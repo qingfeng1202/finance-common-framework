3a70554b31b6c46037ad1375cb265ec92edbcd75
huqingfeng <huqingfeng@azallive.cn>
2026-01-16 10:09:18 +0800

    no message

--- README.md
+++
@@ -1,1 +0,0 @@
-# finance-common-framework
---
+++ src/main/java/com/tigercub/commonframework/FinanceCommonFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.tigercub.commonframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceCommonFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceCommonFrameworkApplication.class, args);
+	}
+
+}
